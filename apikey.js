c7556f296ed39693ae721381d9e859c703c8160d
Christian Lowe <christglowe@gmail.com>
2018-03-13 12:54:50 -0500

    Added yelp api key

--- apikey.js
+++ apikey.js
@@ -4,5 +4,9 @@ const FANDANGO_KEY = "qge5nda35cvnvch2bgnjmdwj";
 const FANDANGO_SECRET = "YhTyVCh642";
 
 
-//The Movie Data Base Api 
+//The Movie Data Base Api
 const TMDB_KEY = "20698cb7955e634443b23540af648b18"
+
+
+//Yelp API
+const YELP_KEY = "ZOF2CYgpCxo8iRohet0kSs1jcdpZmh0Iz2gwJ8gHnqyVoF-InkJiwf6jmKayXTtjZCbFC9WXBQqUJmo5Juo4YnhRTtz0x-lp1dlj6OSsvZZCkwvvkm6IZ4-Y6w-oWnYx"
