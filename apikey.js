439ffda0e7da26f6e2582e4fda9309d83c681297
Christian Lowe <christglowe@gmail.com>
2018-03-13 11:16:52 -0500

    added more keys

--- apikey.js
+++ apikey.js
@@ -1,6 +1,8 @@
-// const KEY = "qge5nda35cvnvch2bgnjmdwj";
-// const SECRET = "YhTyVCh642";
+
+//Fandango api
+const FANDANGO_KEY = "qge5nda35cvnvch2bgnjmdwj";
+const FANDANGO_SECRET = "YhTyVCh642";
 
 
-//The Movie Data Base Api KEY
-const KEY = "20698cb7955e634443b23540af648b18"
+//The Movie Data Base Api 
+const TMDB_KEY = "20698cb7955e634443b23540af648b18"
