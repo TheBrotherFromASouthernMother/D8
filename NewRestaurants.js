8c3dee8573b1c1ea6596dec2a92a12aec6f6dd20
Christian Lowe <christglowe@gmail.com>
2018-03-15 16:38:00 -0500

    added function to handle Zamato response object and turn the data recieved into workable data to be integreated into the UI

--- NewRestaurants.js
+++ NewRestaurants.js
@@ -1,20 +1,20 @@
-var actionAdventure = [73, 150, 1, 227, 193];
+let actionAdventure = [73, 150, 1, 227, 193];
 
-var hipster = [161, 3, 308, 70, 491];
+let hipster = [161, 3, 308, 70, 491];
 
-var romance = [100, 177, 55, 143, 83];
+let romance = [100, 177, 55, 143, 83];
 
-var disney = [25, 304, 996, 82, 168]
+let disney = [25, 304, 996, 82, 168]
 
 
 
 function makeRequest(arr) {
-  var apiKey = "c5d3000aa27c15341e4ef99bcd037e51"
-  var url = `https://developers.zomato.com/api/v2.1/search?entity_id=277&entity_type=city&radius=25000&cuisines=${arr[0]}%2C%20${arr[1]}%2C%20${arr[2]}%2C%20${arr[3]}%2C%20${arr[4]}`
+  let apiKey = "c5d3000aa27c15341e4ef99bcd037e51"
+  let url = `https://developers.zomato.com/api/v2.1/search?entity_id=277&entity_type=city&count=10&radius=25000&cuisines=${arr[0]}%2C%20${arr[1]}%2C%20${arr[2]}%2C%20${arr[3]}%2C%20${arr[4]}&sort=rating`
 
   $.get(url + "&apikey=" + apiKey).done(function(response) {
-    console.log(response)
-    // updateUISuccess(response, arr);
+    console.log(response.restaurants[0])
+    handleResponseObject(response);
     }).fail(function(error) {
         console.log(error);
       })
@@ -22,15 +22,23 @@ function makeRequest(arr) {
 
 
 
-// function updateUISuccess(response, personality) {
-//     var newResponse = response.cuisines.filter(function(element) {
-//         if (personality.includes(element.cuisine.cuisine_id))
-//         console.log(element.cuisine.cuisine_id);
-//             return element.cuisine.cuisine_id;
-//
-//     })
-//
-//
-// }
+function handleResponseObject(data) {
+    let relevantRestaurantData = [];
+		let obj = null;
+		for (let i = 0; i < data.restaurants.length; i ++) {
+			obj = {
+					name: data.restaurants[i].restaurant.name,
+					location: data.restaurants[i].restaurant.location,
+					featuredImage: data.restaurants[i].restaurant.featured_image,
+					// rating: data.restaurants[i].user_rating.aggregate_rating,
+					thumbnail: data.restaurants[i].restaurant.thumb,
+					price: data.restaurants[i].restaurant.price_range
+				}
+			relevantRestaurantData.push(obj)
+		}
+		console.log(relevantRestaurantData)
+		return relevantRestaurantData;
+
+}
 
 makeRequest(actionAdventure)
--- testFolder/zamatoAJAXcalltest.js
+++ testFolder/zamatoAJAXcalltest.js
@@ -1,32 +1,44 @@
+let actionAdventure = [73, 150, 1, 227, 193];
 
-"use strict";
+let hipster = [161, 3, 308, 70, 491];
 
-//Jquery request.
+let romance = [100, 177, 55, 143, 83];
 
-(function() {
+let disney = [25, 304, 996, 82, 168]
 
-	var url = `https://developers.zomato.com/api/v2.1/search?entity_id=277&entity_type=city&apikey=${apiKey}`;
-	var apiKey = "c5d3000aa27c15341e4ef99bcd037e51";
 
-    $.get(url + apiKey).done(function(response) {
-        console.log(response);
 
-        updateUISuccess(response);
+function makeRequest(arr) {
+  let apiKey = "c5d3000aa27c15341e4ef99bcd037e51"
+  let url = `https://developers.zomato.com/api/v2.1/search?entity_id=277&entity_type=city&count=10&radius=25000&cuisines=${arr[0]}%2C%20${arr[1]}%2C%20${arr[2]}%2C%20${arr[3]}%2C%20${arr[4]}&sort=rating`
+
+  $.get(url + "&apikey=" + apiKey).done(function(response) {
+    console.log(response.restaurants[0])
+    handleResponseObject(response);
     }).fail(function(error) {
         console.log(error);
+      })
+}// end makeRequest
 
-        // updateUIError();
-    })
 
-// Handle XHR succes.
 
-function updateUISucces(response) {
-    break;
+function handleResponseObject(data) {
+    let relevantRestaurantData = [];
+		let obj = null;
+		for (let i = 0; i < data.restaurants.length; i ++) {
+			obj = {
+					name: data.restaurants[i].restaurant.name,
+					location: data.restaurants[i].restaurant.location,
+					featuredImage: data.restaurants[i].restaurant.featured_image,
+					// rating: data.restaurants[i].user_rating.aggregate_rating,
+					thumbnail: data.restaurants[i].restaurant.thumb,
+					price: data.restaurants[i].restaurant.price_range
+				}
+			relevantRestaurantData.push(obj)
+		}
+		console.log(relevantRestaurantData)
+		return relevantRestaurantData;
+
 }
-// Handle XHR failure.
 
-// function updateUIError()
-
-
-
-})();
+makeRequest(actionAdventure)
